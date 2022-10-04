e04fe72e4d30f1007305f8b36a386f1d4cdb3d08
Anton <obsos32@gmail.com>
2022-10-04 14:34:46 +0300

    Дипломный проект Foodgram v.21

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,9 +9,9 @@ class IsAdminOrReadOnly(BasePermission):
 
 class IsAdminAuthorOrReadOnly(BasePermission):
 
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
-
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.is_staff
+        return (
+                request.method in ('GET',)
+                or obj.author == request.user
+                or request.user.is_staff
+        )
