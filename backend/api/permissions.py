1e641d31194999f73479a208a5d991b465d27858
Anton <obsos32@gmail.com>
2022-10-05 23:01:56 +0300

    Дипломный проект Foodgram v.34

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+from recipes.models import Recipe
 
 
 class IsAdminOrReadOnly(BasePermission):
@@ -8,6 +11,16 @@ class IsAdminOrReadOnly(BasePermission):
 
 
 class IsAdminAuthorOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        url = request.build_absolute_uri().split('/')
+        ending = url[-1] if url[-1] else url[-2]
+        pk = url[-2] if url[-1] else url[-3]
+        recipe = get_object_or_404(Recipe, id=pk)
+        if ending == 'edit':
+            return recipe.author == request.user or request.user.is_staff
+        else:
+            return request.method in SAFE_METHODS or request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
         url = request.build_absolute_uri().split('/')
