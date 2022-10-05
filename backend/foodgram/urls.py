6f3c673aa80456074880b3a6f1e6b47f019d0f2b
Anton <obsos32@gmail.com>
2022-10-05 19:35:31 +0300

    Дипломный проект Foodgram v.24

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
+from django.shortcuts import get_object_or_404
+
+from recipes.models import Recipe
+
+
+def custom_edit(request, pk):
+    recipe = get_object_or_404(Recipe, id=pk)
+    if request.user.is_authenticated() and recipe.author == request.user:
+        return HttpResponseRedirect(f'/recipes/{pk}/edit/')
+    else:
+        return HttpResponseRedirect('/recipes/')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipes/<pk>/edit/', custom_edit),
     path('api/', include('api.urls')),
     path('api/', include('users.urls')),
 ]
